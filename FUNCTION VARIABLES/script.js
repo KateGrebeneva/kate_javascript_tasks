d60c09de30f1bd5da5fc9428e07e58a81cf41af6
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-14 12:53:48 +0300

    Решение задачи 219

--- FUNCTION VARIABLES/script.js
+++ FUNCTION VARIABLES/script.js
@@ -52,3 +52,68 @@
 // }
 // func();
 // console.log(num); // Результат выполнения задания = 1
+
+// Задание 219 №1
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 219 №2
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 219 №3
+// let num = 1;
+// function func() {
+//   let num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 1 
+
+// Задание 219 №4
+// let num = 1;
+// function func() {
+//   let num = 2;
+// }
+// console.log(num);
+// func(); // Результат выполнения задания = 1 
+
+// Задание 219 №5
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// console.log(num);
+// func(); // Результат выполнения задания = 1 
+
+// Задание 219 №6
+// let num = 1;
+// function func() {
+//   num++;
+// }
+// func();
+// func();
+// func();
+// console.log(num); // Результат выполнения задания = 4
+
+// Задание 219 №7
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// console.log(num);
+// func(); // Результат выполнения задания = 1
+
+// Задание 219 №8
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// func();
+// console.log(num); // Результат выполнения задания = 2
