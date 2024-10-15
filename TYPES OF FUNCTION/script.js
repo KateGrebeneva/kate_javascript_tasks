18d0922ecd8810f1045ca1a89678690128e2ea65
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-15 15:01:02 +0300

    Решение задачи 225

--- TYPES OF FUNCTION/script.js
+++ TYPES OF FUNCTION/script.js
@@ -14,10 +14,23 @@ function func224() {
 // Задание 224 №2
 console.log(func224()); 
   
-// Задание 224 №2
+// Задание 224 №3
 console.log(func224);
   
-// Задание 224 №2
+// Задание 224 №4
 func224 = 123; 
 console.log(func224); 
+
+// Задание 225 №1
+console.log('Результат задания 225:');
+// Задание 1
+function func225_1() {
+	return 3;
+}
   
+// Задание 225 №2
+ const func225_2 = func225_1;
+
+// Задание 225 №3
+console.log(func225_1() + func225_2()); 
+
