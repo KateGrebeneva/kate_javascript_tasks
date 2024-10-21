17a0a44212dc2acbeaf468e0a4ee92f4fcb7417e
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-21 16:16:18 +0300

    Решение задачи 249

--- NESTED FUNCTIONS/tasks#248-256/script.js
+++ NESTED FUNCTIONS/tasks#248-256/script.js
@@ -64,3 +64,59 @@
 // test(1); 
 // Результатом выведения в консоль будет: 2
 
+// Задание 249 №1
+// function test(num) {
+// 	function func(num) {
+// 		console.log(num);
+// 	}
+// 	func(num);
+// }
+// test(1); 
+// Результатом выведения в консоль будет: 1
+
+// Задание 249 №2
+// function test(num) {
+// 	function func(num) {
+// 		num = 2;
+// 	}
+// 	func(num);
+// 	console.log(num);
+// }
+// test(1); 
+// Результатом выведения в консоль будет: 1
+
+// // Задание 249 №3
+// function test(num) {
+// 	function func(num) {
+// 		console.log(num);
+// 	}
+// 	num = 2;
+// 	func(num);
+// }
+// test(1); 
+// Результатом выведения в консоль будет: 2
+
+// Задание 249 №4
+// function test(num) {
+// 	function func(num) {
+// 		console.log(num);
+// 	}
+// 	func(num);
+// 	num = 2;
+// }
+// test(1); 
+// Результатом выведения в консоль будет: 1
+
+// Задание 250 №1
+console.log('Результат задания 250 №1:');
+function func250_1() {
+	return function() {
+		return 1;
+	};
+}
+function func250_2() {
+	return function() {
+		return 2;
+	};
+}
+console.log(func250_1()() + func250_2()());
