76a8dd9cec7525217c34a287358b7f9feff68876
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-24 18:20:09 +0300

    Решение задачи 259

---
+++ CLOSURES/script.js
@@ -0,0 +1,46 @@
+// Задание 259 №1
+// function test() {
+// 	let num1 = 1;
+// 	let num2 = 2;	
+// 	return function() {
+// 		return num1 + num2;
+// 	}
+// }
+// let func = test();
+// console.log(func()); 
+// Результатом выведения в консоль будет: 1
+
+// Задание 259 №2
+// function test() {
+// 	let num1 = 1;
+// 	let num2 = 2;
+// 	return function() {
+// 		return num1 + num2;
+// 	}
+// }
+// console.log(test()()); 
+// Результатом выведения в консоль будет: 3
+
+// Задание 259 №3
+// function test() {
+// 	let num1 = 1;
+// 	return function() {
+// 		return num1 + num2;
+// 	}
+// }
+// let num2 = 2;
+// let func3 = test();
+// console.log(func3()); 
+// Результатом выведения в консоль будет: 3
+
+// Задание 259 №4
+// function test() {
+// 	let num = 1;
+// 	return function() {
+// 		return num;
+// 	}
+// }
+// let num = 2;
+// let func4 = test();
+// console.log(func4());
+// Результатом выведения в консоль будет: 1
