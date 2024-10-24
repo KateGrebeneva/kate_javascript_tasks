0ea6f8544ad7855bbab33ca62ac67f79a2fd2f9e
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-24 18:27:51 +0300

    Решение задачи 261

--- CLOSURES/script.js
+++ CLOSURES/script.js
@@ -44,3 +44,59 @@
 // let func4 = test();
 // console.log(func4());
 // Результатом выведения в консоль будет: 1
+
+// Задание 261 №1
+console.log('Результат задачи 261 №1:');
+function createCounter() {
+	let count = 0; 
+	return function() {
+	  count++; 
+	  console.log(count); 
+	};
+  }
+  let counter = createCounter();
+  counter(); 
+  counter(); 
+  counter(); 
+
+// Задание 261 №2
+console.log('Результат задачи 261 №2:');
+  function createDecrementer() {
+	let number = 10; 
+	return function() {
+	  console.log(number); 
+	  number--; 
+	};
+  }
+  let decrementer = createDecrementer();
+  decrementer(); 
+  decrementer(); 
+  decrementer(); 
+  
+// Задание 261 №3
+console.log('Результат задачи 261 №3:');
+function createCountdown() {
+	let number = 10; 
+	return function() {
+	  if (number >= 0) {
+		console.log(number);
+		number--; 
+	  } else {
+		console.log("Отсчет окончен."); 
+	  }
+	};
+  }
+  let countdown = createCountdown();
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  countdown(); 
+  
