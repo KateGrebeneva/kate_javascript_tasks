29bb31e661927a972011bc91b542db6b79bc249d
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-27 18:43:53 +0300

    Решение задачи 267

--- IIFE/script.js
+++ IIFE/script.js
@@ -18,23 +18,54 @@
 // Результатом выведения в консоль будет: !
 
 // Задание 265 №2
-// let result2 = function() {
+// let result = function() {
 // 	return '!';
 // };
-// console.log(result2) 
+// console.log(result) 
 // Результатом выведения в консоль будет:
 // ƒ () {
 // 	return '!';
 // }
 
 // Задание 265 №3
-// let result3 = function() {
+// let result = function() {
 // 	return '!';
 // };
-// console.log(result3()); 
+// console.log(result()); 
 // Результатом выведения в консоль будет: !
 
 // Задание 266 №1
 // let result = function() {return 1;}() + function() {return 2;}();
 // console.log(result);
 // Результатом выведения в консоль будет: 3
+
+// Задание 267 №1
+// let result = (function() {
+// 	return '!';
+// }());
+// console.log(result); 
+// Результатом выведения в консоль будет: !
+
+// Задание 267 №2
+// let result = (function() {
+// 	return '!';
+// })();
+// console.log(result); 
+// Результатом выведения в консоль будет: !
+
+// Задание 267 №3
+// let result = (function() {
+// 	return '!';
+// });
+// console.log(result);
+// Результатом выведения в консоль будет: 
+// ƒ () {
+// 	return '!';
+// }
+
+// Задание 267 №4
+// let result = (function() {
+// 	return '!';
+// });
+// console.log(result()); 
+// Результатом выведения в консоль будет: !
