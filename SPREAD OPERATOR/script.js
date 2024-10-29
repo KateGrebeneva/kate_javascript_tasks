3d78e780c3acaa12f9417616f09a0f08cd162749
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-29 10:06:34 +0300

    Решение задачи 287

--- SPREAD OPERATOR/script.js
+++ SPREAD OPERATOR/script.js
@@ -5,3 +5,10 @@ function func(num1, num2, num3, num4, num5) {
 	return num1 + num2 + num3 + num4 + num5;
 }
 console.log(func(...arr286));
+
+// Задание 287 №1
+// function func(n1, n2, n3, n4, n5, n6, n7, n8) {
+// 	return (n1 + n2 + n3 + n4) * (n5 + n6 + n7 + n8);
+// }
+// console.log( func(1, ...[2, 3, 4], 5, ...[6], ...[7, 8]) ); 
+// Результатом выведения в консоль будет: 260
