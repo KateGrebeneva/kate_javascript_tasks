82cdfc210e502ae4f657c64800e0607a111a0e2b
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-30 17:55:35 +0300

    Изучение и отработка задачи 295

--- REST OPERATOR/script.js
+++ REST OPERATOR/script.js
@@ -11,3 +11,18 @@ function func293(...nums) {
 }
 let result293 = func293(15, 5, 10, 25, 55);
 console.log(result293);
+
+// Задание 295 №1
+console.log('Результат задачи 295 №1:');
+merge1([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+function merge1(...arrs1){
+	console.log(arrs1); 
+}
+
+// Задание 295 №2
+console.log('Результат задачи 295 №2:');
+function merge2(...arrs2) {
+	return [].concat(...arrs2);
+}
+let result2 = merge2([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+console.log(result2); 
