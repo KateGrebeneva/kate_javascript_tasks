f498d0e6e901d875c08db0c420b36ede204b8830
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-30 18:18:24 +0300

    Решение задачи 296

---
+++ DESTRUCTURING/script.js
@@ -0,0 +1,5 @@
+// Задание 296 №1
+console.log('Результат задачи 296 №1:');
+let arr296 = ['John', 'Smit', 'development', 'programmer', 2000];
+let [first_name, surname, department, position, salary] = arr296; 
+console.log(first_name, surname, department, position, salary);
