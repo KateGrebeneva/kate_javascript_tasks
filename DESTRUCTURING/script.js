27291fa93ca3a0794e1193f23c75654885f1f78b
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-10-30 18:25:35 +0300

    Решение задачи 297

--- DESTRUCTURING/script.js
+++ DESTRUCTURING/script.js
@@ -3,3 +3,11 @@ console.log('Результат задачи 296 №1:');
 let arr296 = ['John', 'Smit', 'development', 'programmer', 2000];
 let [first_name, surname, department, position, salary] = arr296; 
 console.log(first_name, surname, department, position, salary);
+
+// Задание 297 №1
+console.log('Результат задачи 297 №1:');
+function func297() {
+	return ['Chip and Dale: Rescue Rangers', 'Series', 'Zipper', 'Disney', 1989];
+}
+let [movie, genre, hero, production, year] = func297(); 
+console.log(func297());  
