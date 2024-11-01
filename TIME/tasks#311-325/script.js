1cb56c561dfbaba20cf830fe8ed319f66c591a5c
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-11-01 17:08:49 +0300

    Решение задачи 311

---
+++ TIME/tasks#311-325/script.js
@@ -0,0 +1,9 @@
+// Задание 311 №1
+console.log('Результат задачи 311:');
+let date311 = new Date();
+console.log('Текущий день: ' + (date311.getDate()));   
+// Задание 311 №2  
+console.log('Текущий месяц: ' + (date311.getMonth()));  
+// Задание 311 №3 
+console.log('Текущий год: ' + (date311.getFullYear())); 
+
