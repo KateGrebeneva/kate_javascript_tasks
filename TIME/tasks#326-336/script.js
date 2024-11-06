62c70bfbbc3d233cc18baa68adc983184faa2081
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-11-06 15:14:29 +0300

    Решение задачи 332

--- TIME/tasks#326-336/script.js
+++ TIME/tasks#326-336/script.js
@@ -75,3 +75,77 @@ for (let year = startYear; year <= currentYear; year++) {
 }
 }
 console.log('Количество раз, когда 1 января попадало на выходной день с 2000 года до ' + (currentYear)+  ' : ' + (weekendCount));
+
+// Задание 332 - ПРАКТИКА
+// №1
+console.log('Результат задачи 332 №1:');
+now = new Date();
+startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+const secondsSinceStartOfDay = Math.floor((now - startOfDay) / 1000);
+console.log('Секунд с начала дня: ' + (secondsSinceStartOfDay));
+
+// №2
+console.log('Результат задачи 332 №2:');
+endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+const secondsUntilEndOfDay = Math.floor((endOfDay - now) / 1000);
+console.log('Секунд до конца дня: ' + (secondsUntilEndOfDay));
+
+// №3
+console.log('Результат задачи 332 №3:');
+nextYear = new Date(now.getFullYear() + 1, 0, 1);
+const daysUntilNewYear = Math.ceil((nextYear - now) / (1000 * 60 * 60 * 24));
+console.log('Дней до нового года: ' + (daysUntilNewYear));
+
+// №4
+console.log('Результат задачи 332 №4:');
+let fridaysThe13thCount = 0;
+for (let month = 0; month < 12; month++) {
+    const date = new Date(now.getFullYear(), month, 13);
+    if (date.getDay() === 5) { // 5 - пятница
+        fridaysThe13thCount++;
+    }
+}
+console.log('Количество пятниц 13-х в текущем году: ' + (fridaysThe13thCount));
+
+// №5
+console.log('Результат задачи 332 №5:');
+const threeMonthsAgo = new Date(now);
+threeMonthsAgo.setMonth(now.getMonth() - 3);
+console.log('3 месяца назад был год: ' + (threeMonthsAgo.getFullYear()));
+
+// №6
+console.log('Результат задачи 332 №6:');
+now = new Date();
+const lastDayOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const dayOfWeekLastDay = daysOfWeek[lastDayOfMonth.getDay()];
+console.log('Последний день текущего месяца будет: ' + (dayOfWeekLastDay));
+
+// №7
+console.log('Результат задачи 332 №7:');
+now = new Date();
+const year = now.getFullYear();
+const isLeapYear = (year) => {
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+};
+if (isLeapYear(year)) {
+  console.log('Текущий год ' + (year) + ' високосный.');
+} else {
+  console.log('Текущий год ' + (year) + ' не високосный.');
+}
+
+// №8
+console.log('Результат задачи 332 №8:');
+let previousLeapYear = now.getFullYear() - 1;
+while (!isLeapYear(previousLeapYear)) {
+    previousLeapYear--;
+}
+console.log('Предыдущий високосный год: ' + (previousLeapYear));
+
+// №9
+console.log('Результат задачи 332 №9:');
+let nextLeapYear = now.getFullYear() + 1;
+while (!isLeapYear(nextLeapYear)) {
+    nextLeapYear++;
+}
+console.log('Следующий високосный год: ' + (nextLeapYear));
