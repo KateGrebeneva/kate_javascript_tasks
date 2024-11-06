8a328b3ca5b1f6dde172e70360ee72d87af1a54d
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-11-06 14:39:04 +0300

    Решение задачи 327

--- TIME/tasks#326-336/script.js
+++ TIME/tasks#326-336/script.js
@@ -18,3 +18,19 @@ console.log('Результат задачи 326 №3:');
 let dateLastYearSameDate = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
 let dayOfWeekLastYearSameDate = dateLastYearSameDate.getDay();
 console.log('Год назад в такую же дату был: ' + days[dayOfWeekLastYearSameDate]);
+
+// Задание 327 №1
+console.log('Результат задачи 327 №1:');
+let startDate = new Date(new Date().getFullYear(), 0, 1); 
+let endDate = new Date(new Date().getFullYear(), 8, 10); 
+let daysBetween = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24));
+console.log('Количество дней между 1 января и 10 сентября текущего года: ' + (daysBetween));
+
+// Задание 327 №2
+console.log('Результат задачи 327 №2:');
+let currentDate = new Date();
+let startOfNextMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 10); 
+let startOfCurrentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 20); 
+let daysBetweenCurrentAndNext = Math.floor((startOfNextMonth - startOfCurrentMonth) / (1000 * 60 * 60 * 24));
+console.log('Количество дней между 20 числом текущего месяца и 10 числом следующего: ' + (daysBetweenCurrentAndNext));
+
